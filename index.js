b1cd92d47f000938509ec5ff136f7ac3ca6c1aa4
minazul01 <minajul4010@gmail.com>
2025-05-03 10:15:33 +0600

    api create

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const port = process.env.PORT || 8000;
+
+
+// pass: vLR4FHAhmW38Pvwp
 
 // midleware
 app.use(cors());
 app.use(express.json());
+
+
+// console.log(process.env.pass, process.env.user)
+
+const uri = `mongodb+srv://${process.env.user}:${process.env.pass}@cluster0.eyk5ydv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const movieCollection = client.db("movie-feature").collection("feature");
+
+    // movie features to postman creation
+  app.post('/features', async (req, res) => {
+      const data = req.body;
+      const result = await movieCollection.insertOne(data);
+      res.send(result);
+  })
+  // movie features data client site show
+  app.get('/features', async (req, res) => {
+    const result = await movieCollection.find().toArray();
+    res.send(result);
+  })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {

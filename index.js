5019e40285670df9e928b3d16ebd27e5d5c732e3
minazul01 <minajul4010@gmail.com>
2025-05-06 20:39:45 +0600

    recap favourite movie

--- index.js
+++ index.js
@@ -71,8 +71,9 @@ async function run() {
   // favourite movie delete
   app.delete('/favourites/:id', async (req, res) => {
     const id = req.params.id;
-    const query = {_id: new ObjectId(id)};
+    const query = {_id:id};
     const result = await favouriteCollection.deleteOne(query);
+    console.log(result)
     res.send(result);
     
   })
